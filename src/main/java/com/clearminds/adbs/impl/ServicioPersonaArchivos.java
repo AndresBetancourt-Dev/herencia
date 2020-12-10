afa79307e567ce13e9195a92ceb3f68f4ca2816e
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-12-10 10:29:13 -0500

    Modified Person Archives Service

--- src/main/java/com/clearminds/adbs/impl/ServicioPersonaArchivos.java
+++ src/main/java/com/clearminds/adbs/impl/ServicioPersonaArchivos.java
@@ -7,19 +7,19 @@ public class ServicioPersonaArchivos implements ServicioPersona{
 
 
 	public boolean insertar(Persona persona) {
-		System.out.println("Insertando persona en la Base de Datos.");
+		System.out.println("Insertando persona en un archivo.");
 		System.out.println(persona.toString());
 		return true;
 	}
 
 	public boolean actualizar(Persona persona) {
-		System.out.println("Actualizando persona en la Base de Datos.");
+		System.out.println("Actualizando persona en un archivo.");
 		System.out.println(persona.toString());
 		return true;
 	}
 
 	public boolean eliminar(Persona persona) {
-		System.out.println("Eliminando persona en la Base de Datos.");
+		System.out.println("Eliminando persona en un archivo.");
 		System.out.println(persona.toString());
 		return true;
 	}
